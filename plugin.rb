a9781a4719fd59a0968a4fac17a313eb943c0d32
Bolarinwa Balogun <bo.lariin@hotmail.com>
2017-07-31 22:58:04 -0400

    Add necessary indents and missing end statement

--- plugin.rb
+++ plugin.rb
@@ -14,17 +14,18 @@ after_initialize do
 
     # user created
     if !user
-    response_username = "student_response"
-    response_name = "Student Reponse"
-    
-    user = User.new
-    user.id = -10
-    user.name = response_name
-    user.username = response_username
-    user.email = "student_response@me.com"
-    user.username_lower = response_username.downcase
-    user.password = SecureRandom.hex
-    user.active = true
-    user.approved = true
-    user.trust_level = TrustLevel[1]
+        response_username = "student_response"
+        response_name = "Student Reponse"
+        
+        user = User.new
+        user.id = -10
+        user.name = response_name
+        user.username = response_username
+        user.email = "student_response@me.com"
+        user.username_lower = response_username.downcase
+        user.password = SecureRandom.hex
+        user.active = true
+        user.approved = true
+        user.trust_level = TrustLevel[1]
     end
+end
