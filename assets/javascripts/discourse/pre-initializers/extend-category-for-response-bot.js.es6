48f2f5fe184f5cf20babf5dee1b090c5edeb8b46
Bolarinwa Balogun <bo.lariin@hotmail.com>
2017-08-04 21:57:31 -0400

    Add feature that allows response bot on select categories
    The feature allows staff or users with appropriate trust level
    to enable response by the response bot on select categories.
    This feature allows the respond bot reply new topics within
    a select category. The feature is available to select when
    the "allow response bot to reply all new topics" setting
    is unchecked.

---
+++ assets/javascripts/discourse/pre-initializers/extend-category-for-response-bot.js.es6
@@ -0,0 +1,25 @@
+import property from 'ember-addons/ember-computed-decorators';
+import Category from 'discourse/models/category';
+
+export default {
+  name: 'extend-category-for-response-bot',
+  before: 'inject-discourse-objects',
+  initialize() {
+
+    Category.reopen({
+
+      @property('custom_fields.enable_response_bot')
+      enable_response_bot: {
+        get(enableField) {
+          return enableField === "true";
+        },
+        set(value) {
+          value = value ? "true" : "false";
+          this.set("custom_fields.enable_response_bot", value);
+          return value;
+        }
+      }
+    });
+  }
+};
+
--- plugin.rb
+++ plugin.rb
@@ -7,32 +7,69 @@
 enabled_site_setting :response_enabled
 
 after_initialize do
-    
-    if SiteSetting.response_enabled
-        # Check if user already exists
-        # using a negative number to ensure it is unique
-        user = User.find_by(id: -10)
 
-        # user created
-        if !user
-            response_username = "student_response"
-            response_name = "Student Reponse"
-            
-            user = User.new
-            user.id = -10
-            user.name = response_name
-            user.username = response_username
-            user.email = "student_response@me.com"
-            user.username_lower = response_username.downcase
-            user.password = SecureRandom.hex
-            user.active = true
-            user.approved = true
-            user.trust_level = TrustLevel[1]
+    class ::Category
+
+        def self.reset_response_cache
+            @@enable_response_cache["allowed"] =
+            begin
+                Set.new(
+                    CategoryCustomField
+                    .where(name: "enable_response_bot", value: "true")
+                    .pluck(:category_id)
+                )
+            end
+        end
+  
+        @@enable_response_cache = DistributedCache.new("enable_response")
+  
+        def self.is_response_bot_enabled_on_category?(category_id)
+            return true if SiteSetting.allow_response_bot_to_reply_all_new_topics
+
+            unless set = @@enable_response_cache["allowed"]
+                set = ::Category.reset_response_cache
+            end
+            set.include?(category_id)
         end
 
-        # event listener for creation of new topic
-        # once a topic is created, automatically reply topic with wiki post
-        DiscourseEvent.on(:topic_created) do |topic|
+        def self.can_respond?(topic)
+            SiteSetting.response_enabled && ::Category.is_response_bot_enabled_on_category?(topic.category_id) && (!topic.closed?)
+        end
+  
+        after_save :reset_response_cache
+  
+        protected
+        def reset_response_cache
+          ::Category.reset_response_cache
+        end
+    end
+  
+
+    # Check if user already exists
+    # using a negative number to ensure it is unique
+    user = User.find_by(id: -10)
+
+    # user created
+    if !user
+        response_username = "student_response"
+        response_name = "Student Reponse"
+        
+        user = User.new
+        user.id = -10
+        user.name = response_name
+        user.username = response_username
+        user.email = "student_response@me.com"
+        user.username_lower = response_username.downcase
+        user.password = SecureRandom.hex
+        user.active = true
+        user.approved = true
+        user.trust_level = TrustLevel[1]
+    end
+
+    # event listener for creation of new topic
+    # once a topic is created, automatically reply topic with wiki post
+    DiscourseEvent.on(:topic_created) do |topic|
+        if ::Category.can_respond?(topic)
             post = PostCreator.create(user,
                         topic_id: topic.id,
                         raw: I18n.t('bot.default_message'))
